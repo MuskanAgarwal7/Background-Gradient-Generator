82ecbc602e739008fdca03ef4d67f202fae1132f
Muskan Agarwal <muskaninjhansi@gmail.com>
2025-03-22 13:06:49 +0530

    update index, script, style files

--- script.js
+++ script.js
@@ -2,34 +2,38 @@ let btn1 = document.querySelector("#btn1");
 let btn2 = document.querySelector("#btn2");
 let copyCode = document.querySelector(".copy-code");
 
-const handleBtn1 = () => {
-  let randomColor1 = `#${Math.random().toString(16).slice(2, 8)}`;
+let color1 = "#5c91e5"; // Initial color
+let color2 = "#983fc6"; // Initial color
 
-  btn1.innerText = randomColor1;
-  btn1.style.backgroundColor = randomColor1;
-  document.body.style.backgroundImage = `linear-gradient(
-    to right,
-    ${randomColor1},
-    #444
-    )`;
-  copyCode.innerText = document.body.style.backgroundImage;
+const generateRandomColor = () => {
+  return `#${Math.random().toString(16).slice(2, 8).padEnd(6, "0")}`;
+};
+
+const updateGradient = () => {
+  document.body.style.backgroundImage = `linear-gradient(to right, ${color1}, ${color2})`;
+  copyCode.innerText = `background-image: linear-gradient(to right, ${color1}, ${color2});`;
+};
+
+const handleBtn1 = () => {
+  color1 = generateRandomColor();
+
+  btn1.innerText = color1;
+  btn1.style.backgroundColor = color1;
+  updateGradient();
 };
 
 const handleBtn2 = () => {
-  let randomColor2 = `#${Math.random().toString(16).slice(2, 8)}`;
+  color2 = generateRandomColor();
 
-  btn2.innerText = randomColor2;
-  btn2.style.backgroundColor = randomColor2;
-  document.body.style.backgroundImage = `linear-gradient(
-    to right,
-    #444,
-    ${randomColor2}
-    )`;
-  copyCode.innerText = document.body.style.backgroundImage;
+  btn2.innerText = color2;
+  btn2.style.backgroundColor = color2;
+  updateGradient();
 };
 
-const gradient = () => {};
+copyCode.addEventListener("click", () => {
+  navigator.clipboard.writeText(copyCode.innerText);
+  alert("Copied to clipboard! 🎉");
+});
 
 btn1.addEventListener("click", handleBtn1);
 btn2.addEventListener("click", handleBtn2);
-// copyCode.addEventListener()
